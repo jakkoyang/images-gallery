d9822cb12f3a8053ab0d09e75e61c5c317cb079b
Jakko <jakkohyang@gmail.com>
2024-11-09 13:03:57 -0600

    cors error and imageCard fixed

--- api/main.py
+++ api/main.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 import requests
 import os
 from dotenv import load_dotenv
+from flask_cors import CORS
 
 load_dotenv(dotenv_path="./.env.local")
 
@@ -13,6 +14,8 @@ if not UNSPLASH_KEY:
     raise EnvironmentError("Please create .env.local file and create UNSPLASH_KEY")
 
 app = Flask(__name__)
+CORS(app)
+
 app.config["DEBUG"] = DEBUG
 
 @app.route("/new-image")
--- frontend/src/components/ImageCard.js
+++ frontend/src/components/ImageCard.js
@@ -4,10 +4,12 @@ import { Card, Button } from "react-bootstrap";
 const ImageCard = ({ image, deleteImage }) => {
   return (
     <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src={image.urls.small} />
+      <Card.Img variant="top" src={image.data.urls.small} />
       <Card.Body>
         <Card.Title>{image.title.toUpperCase()}</Card.Title>
-        <Card.Text>{image.description || image.alt_description}</Card.Text>
+        <Card.Text>
+          {image.data.description || image.data.alt_description}
+        </Card.Text>
         <Button variant="primary" onClick={() => deleteImage(image.id)}>
           Delete
         </Button>
